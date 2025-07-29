88e1eb5d1f4bb02f3bf3278b12615add867f55d5
beetlejaws <pavelsarych@gmail.com>
2025-07-29 16:53:30 +0300

    started project with poetry

---
+++ config.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from environs import Env
+
+@dataclass
+class Bot:
+    token: str
+
+@dataclass
+class Db:
+    name: str
+    host: str
+    user: str
+    password: str
+
+    def __post_init__(self):
+        self.url = f'postgresql+asyncpg://{self.user}:{self.password}@{self.host}/{self.name}'
+
+@dataclass
+class Config:
+    bot: Bot
+    db: Db
+
+
+def load_config(path: str | None = None) -> Config:
+    env: Env = Env()
+    env.read_env(path)
+
+    return Config(
+        bot=Bot(
+            token=env('BOT_TOKEN')
+        ),
+        db=Db(
+            name=env('DB_NAME'),
+            host=env('DB_HOST'),
+            user=env('DB_USER'),
+            password=env('DB_PASSWORD')
+        )
+    )
