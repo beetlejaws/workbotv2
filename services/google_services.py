21b403c29b0b2851663fec4eb3ab5df572a129eb
beetlejaws <pavelsarych@gmail.com>
2025-07-31 16:56:46 +0300

    added google sheets logic

---
+++ services/google_services.py
@@ -0,0 +1,34 @@
+import aiohttp
+from google.oauth2.service_account import Credentials
+from google.auth.transport.requests import Request
+
+
+class GoogleService:
+    def __init__(self, credentials_path: str, scopes: list[str]) -> None:
+        self.creds = Credentials.from_service_account_file(
+            credentials_path, scopes=scopes
+        )
+        self.creds.refresh(Request())
+        self.access_token = self.creds.token
+
+
+class GoogleSheets(GoogleService):
+    def __init__(self, credentials_path: str):
+        scopes = ['https://www.googleapis.com/auth/spreadsheets']
+        super().__init__(credentials_path, scopes)
+        
+
+    async def get_sheet_data(self, sheet_id: str) -> list:
+        url = f'https://sheets.googleapis.com/v4/spreadsheets/{sheet_id}/values/Sheet1!A:Z'
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+
+        if not self.creds.valid:
+            self.creds.refresh(Request())
+            self.access_token = self.creds.token
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers) as response:
+                data = await response.json()
+                print(data['values'])
+                return data['values']
+
